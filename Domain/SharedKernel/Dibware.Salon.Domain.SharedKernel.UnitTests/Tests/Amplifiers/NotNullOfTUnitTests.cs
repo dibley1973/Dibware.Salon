f055ff32bc67f13b7fa158aab6769f5d6cdcb5ab
Duane Wingett <dibley1973@yahoo.co.uk>
2019-04-12 06:11:32 +0100

    Added SharedKernel test project dependencies

---
+++ Domain/SharedKernel/Dibware.Salon.Domain.SharedKernel.UnitTests/Tests/Amplifiers/NotNullOfTUnitTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Dibware.Salon.Domain.SharedKernel.Amplifiers;
+using Dibware.Salon.Domain.SharedKernel.UnitTests.Fakes;
+using Dibware.Salon.Domain.SharedKernel.UnitTests.TestData;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Dibware.Salon.Domain.SharedKernel.UnitTests.Tests.Amplifiers
+{
+    /// <summary>
+    /// Provides unit tests for the <see cref="Entity{TId}"/> class.
+    /// </summary>
+    [TestFixture]
+    public class NotNullOfTUnitTests
+    {
+        [Test]
+        public void GivenConstructor_WhenPassedNull_ThenThrowsException()
+        {
+            // ARRANGE
+            const FakeEntity nullEntity = null;
+            
+            // ACT
+            Action actual = () => new NotNull<FakeEntity>(nullEntity);
+
+            // ASSERT
+            actual.Should().Throw<ArgumentNullException>("because a null object is not permitted");
+        }
+
+    }
+}
