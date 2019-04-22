51f3ce7292627b7f1f0190b6f5606da160f9271e
Duane Wingett <dibley1973@yahoo.co.uk>
2019-04-22 07:14:43 +0100

    Could not find any coverage report file matching the patternort filtering to try and find report. #5

--- run-tests.sh
+++ run-tests.sh
@@ -1,3 +1,2 @@
 # Run the tests and collate code coverage results
-#dotnet test -c Release --no-build --no-restore Domain/SharedKernel/Dibware.Salon.Domain.SharedKernel.UnitTests/Dibware.Salon.Domain.SharedKernel.UnitTests.csproj /p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:CoverletOutput=../lcov
 dotnet test -c Release --no-build --no-restore Domain/SharedKernel/Dibware.Salon.Domain.SharedKernel.UnitTests/Dibware.Salon.Domain.SharedKernel.UnitTests.csproj /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
